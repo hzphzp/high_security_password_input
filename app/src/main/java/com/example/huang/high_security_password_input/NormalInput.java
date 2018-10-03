559cfc2682cde2ee28d92c562b118a83914d8603
hzphzp <huangzp1104@gmail.com>
2018-10-03 17:12:28 +0800

    create a new activity

---
+++ app/src/main/java/com/example/huang/high_security_password_input/NormalInput.java
@@ -0,0 +1,13 @@
+package com.example.huang.high_security_password_input;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NormalInput extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_normal_input);
+    }
+}
